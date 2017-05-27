c2a88c82c2d4c753b1d9d576bee7f68056f27b49
Nayab Siddiqui <nayab.s86@gmail.com>
2017-05-27 12:11:07 +0530

    getting rid of unnecessary companion objects for events

--- app/event/EmployeeEvent.scala
+++ app/event/EmployeeEvent.scala
@@ -1,7 +1,6 @@
 package event
 
 import org.joda.time.DateTime
-import play.api.libs.json.Json
 
 sealed trait EmployeeEvent extends Event
 
@@ -10,19 +9,5 @@ case class EmployeeRegistered(firstName: String, lastName: String) extends Emplo
 case class LeavesCredited(creditedLeaves: Float) extends EmployeeEvent
 
 case class LeavesApplied(from: DateTime, to: DateTime, isHalfDay: Boolean) extends EmployeeEvent
-
-
-object EmployeeRegistered {
-  implicit val employeeRegisteredFormat = Json.format[EmployeeRegistered]
-}
-
-object LeavesCredited {
-  implicit val leavesCreditedFormat = Json.format[LeavesCredited]
-}
-
-object LeavesApplied {
-  implicit val fullDayLeavesApplied = Json.format[LeavesApplied]
-}
-
 
 
